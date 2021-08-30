d08764feb8c659de91268ac23ea5bbed218857d2
iyanuoluwa <iyanu@kenshomedia.com>
2021-08-30 21:52:12 +0100

    conscious effort to code only in typescript from now

--- js/cardio/cardio.js
+++
@@ -1,9 +0,0 @@
-const reverse = (str) => {
-  const arr = str.split('');
-  const result = arr.map((char, i, arr) => arr[arr.length - 1 - i]).join('');
-
-  return result;
-};
-
-const output = reverse('oyinkan');
-console.log(output);
--- python/README.md
+++
@@ -1,3 +0,0 @@
-<h3 align='center'> Project 1 (games.py) </h3>
-<p>build a number guessing game, simple but required serious logic
-super proud</p>
