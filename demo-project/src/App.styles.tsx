1ba62f7a2be91b6955bfa9f0a9e5565d40840bc1
iyanuoluwa <iyanu@kenshomedia.com>
2021-09-01 22:29:58 +0100

    practice typescript project

---
+++ demo-project/src/App.styles.tsx
@@ -0,0 +1,8 @@
+import { Box } from '@material-ui/core';
+import styled from 'styled-components';
+
+export const StyledWrapper = styled.div``;
+export const StyledBox = styled(Box)`
+  max-width: 400px;
+  margin: 0 auto;
+`;
