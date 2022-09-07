8eb25241215eb855c2e6cc1990e02556bd14b81b
iyanuoluwa sowande <iyanusowande@gmail.com>
2022-09-07 09:24:38 +0100

    dsa update

--- LinkedList/linkedlist.py
+++ LinkedList/linkedlist.py
@@ -6,13 +6,25 @@ building out the constructor of a linked list
 # create a Node constructor
 
 class Node:
-    def __init__(self, value):
-        self.value = value
+	def __init__(self, value: object) -> object:
+		self.value = value
+		self.next = None
 
 
 class LinkedList:
-    def __init__(self, value):
-        new_node = Node(value)
-        self.head = new_node
-        self.tail = new_node
-        self.length = 1
+	def __init__(self, value):
+		new_node = Node(value)
+		self.head = new_node
+		self.tail = new_node
+		self.length = 1
+
+	def prepend(self, value):
+		new_node = Node(value)
+		if self.length == 0:
+			self.head = new_node
+			self.tail = new_node
+		else:
+			new_node.next = self.head
+			self.head = new_node
+		self.length += 1
+		return True
