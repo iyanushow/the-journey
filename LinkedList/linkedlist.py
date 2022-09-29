331004b0712449816a4b31330625ccb70e8a7736
iyanuoluwa sowande <iyanusowande@gmail.com>
2022-09-29 10:11:45 +0100

    linkedlist update

--- LinkedList/linkedlist.py
+++ LinkedList/linkedlist.py
@@ -90,6 +90,45 @@ class LinkedList:
         else:
             return False
 
+    def insert(self, index, value):
+        if index < 0 or index >= self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get(index - 1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
+
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length:
+            return self.pop()
+        prev = self.get(index - 1)
+        temp = prev.next
+        prev.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
+
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        before, after = None, temp.next
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+
 
 
 linked = LinkedList(1)
@@ -99,9 +138,8 @@ linked.append(3)
 linked.append(4)
 linked.append(5)
 
-# linked.print_list()
-print(linked.get(3).value, 'get')
+linked.reverse()
+linked.print_list()
 
-print(linked.set_value(3, 9), 'set')
 
-print(linked.get(3).value, 'get')
+# print(linked.get(3).value, 'get')
