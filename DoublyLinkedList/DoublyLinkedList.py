58f1255316d0b952495171c018a5c189835267ee
iyanuoluwa sowande <iyanusowande@gmail.com>
2022-10-19 07:07:21 +0100

    dll updates

--- DoublyLinkedList/DoublyLinkedList.py
+++ DoublyLinkedList/DoublyLinkedList.py
@@ -41,11 +41,65 @@ class DoublyLinkedList:
         self.length -= 1
         return temp
 
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = self.tail = new_node
+        else:
+            temp = new_node
+            temp.next = self.head
+            self.head.prev = temp
+            self.head = temp
+        self.length += 1
+        return True
+
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = temp.next
+        if self.length == 1:
+            self.tail = None
+        else:
+            self.head.prev = None
+            temp.next = None
+        self.length -= 1
+        return temp
+
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index < self.length / 2:
+            temp = self.head
+            for _ in range(index):
+                temp = temp.next
+            return temp
+        else:
+            temp = self.tail
+            for _ in range(self.length - 1, index, -1):
+                temp = temp.prev
+            return temp
+
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+
+
+# TESTS
 
 new_dll = DoublyLinkedList(12)
-
-# new_dll.append(14)
-# new_dll.append(13)
+#
+new_dll.append(13)
+new_dll.append(14)
 
 new_dll.print_list()
+# new_dll.pop()
+print("###")
+# new_dll.pop_first()
+# new_dll.print_list()
 
+print(new_dll.get(1).value)
+print(new_dll.get(2).value)
